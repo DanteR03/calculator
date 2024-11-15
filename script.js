0948236c8e134f11e845464c419cfdd3ab7b1223
Lev <101640837+DanteR03@users.noreply.github.com.>
2024-11-15 20:00:15 +0500

    Make calculator work with buttons and display operations and results

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 let number1 = 0;
 let number2 = 0;
 let operator = "";
+const calculatorDisplay = document.querySelector("#calculator-display");
+const calculatorButtons = document.querySelectorAll(".calculator-button");
+
+calculatorButtons.forEach((button) => {
+    button.addEventListener("click", pressButton);
+})
+
+function populateDisplay() {
+    if (operator === "") {
+        calculatorDisplay.textContent = number1;
+    } else {
+        calculatorDisplay.textContent = number1 + ` ${operator} ` + number2;
+    }
+}
+
+function pressButton(button) {
+    let buttonContent = button.target.textContent;
+    if (buttonContent === "C") {
+        number1 = 0;
+        number2 = 0;
+        operator = "";
+    } else if (buttonContent === "=") {
+        number1 = operate(number1, number2, operator);
+        number2 = 0;
+        operator = "";
+    } else if (Number.isInteger(+buttonContent) && operator === "") {
+        number1 = +buttonContent;
+    } else if (Number.isInteger(+buttonContent) && operator !== "") {
+        number2 = +buttonContent;
+    } else {
+        operator = buttonContent;
+    };
+    populateDisplay();
+}
 
 function addNums(num1, num2) {
     return num1 + num2;
@@ -26,10 +60,10 @@ function operate(num1, num2, operator) {
         case "-":
             return subtractNums(num1, num2);
             break;
-        case "*":
+        case "x":
             return multiplyNums(num1, num2);
             break;
-        case "/":
+        case "÷":
             return divideNums(num1, num2);
             break;
     };

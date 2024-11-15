89bf85434b25572e47f501059c9da185800ff0dc
Lev <101640837+DanteR03@users.noreply.github.com.>
2024-11-15 18:47:38 +0500

    Write a function that performs one of the operations based on parameters

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-function addNums (num1, num2) {
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
+function addNums(num1, num2) {
     return num1 + num2;
 }
 
-function subtractNums (num1, num2) {
+function subtractNums(num1, num2) {
     return num1 - num2;
 }
 
-function multiplyNums (num1, num2) {
+function multiplyNums(num1, num2) {
     return num1 * num2;
 }
 
-function divideNums (num1, num2) {
+function divideNums(num1, num2) {
     return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return addNums(num1, num2);
+            break;
+        case "-":
+            return subtractNums(num1, num2);
+            break;
+        case "*":
+            return multiplyNums(num1, num2);
+            break;
+        case "/":
+            return divideNums(num1, num2);
+            break;
+    };
 }
